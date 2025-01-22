1da90692f0d9903918d3882980271384bbb45e6a
Kizari <25322543+Kizari@users.noreply.github.com>
2025-01-23 09:48:27 +1300

    test: setup basic test project
    Adds a simple Boost.Test project to the tests directory with a dummy test case.

---
+++ tests/Replica/SQEX/Luminous/AssetManager/LmAssetIDTest.cpp
@@ -0,0 +1,15 @@
+﻿#ifndef LMASSETIDTEST_H
+#define LMASSETIDTEST_H
+
+#define BOOST_TEST_MODULE LmAssetIDTest
+#include "../../../../src/Replica/SQEX/Luminous/AssetManager/LmAssetID.h"
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(InitialValueIsZero)
+{
+    const auto id =
+        SQEX::Luminous::AssetManager::LmAssetID::Create("data://test.ebex@");
+    BOOST_CHECK_EQUAL(0, id.fullHash_);
+}
+
+#endif // LMASSETIDTEST_H
